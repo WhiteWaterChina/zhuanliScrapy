3703bcc455e4fabe20433851ee31dec4ccc8a555
WhiteWaterChina <yanshuo5091@163.com>
2017-06-21 07:33:27 +0800

    20170621 修改GUI策略

--- zhuanliScrapy.py
+++ zhuanliScrapy.py
@@ -3,7 +3,6 @@
 # Author:yanshuo@inspur.com
 
 import wx
-import wx.xrc
 import time
 import os
 from threading import Thread
@@ -20,19 +19,24 @@ import re
 
 class FrameZhuanli(wx.Frame):
     def __init__(self, parent):
-        wx.Frame.__init__(self, parent, id=wx.ID_ANY, title=u"专利信息扒取系统", pos=wx.DefaultPosition, size=wx.Size(393, 297),
+        wx.Frame.__init__(self, parent, id=wx.ID_ANY, title=u"专利信息扒取系统", pos=wx.DefaultPosition, size=wx.Size(393, 316),
                           style=wx.DEFAULT_FRAME_STYLE | wx.TAB_TRAVERSAL)
 
-        self.SetSizeHints(wx.DefaultSize, wx.DefaultSize)
-#        self.SetFont(wx.Font(9, 70, 90, 90, False, "宋体"))
+        self.SetSizeHintsSz(wx.DefaultSize, wx.DefaultSize)
+        self.SetFont(wx.Font(wx.NORMAL_FONT.GetPointSize(), 70, 90, 90, False, wx.EmptyString))
         self.SetForegroundColour(wx.SystemSettings.GetColour(wx.SYS_COLOUR_WINDOW))
         self.SetBackgroundColour(wx.SystemSettings.GetColour(wx.SYS_COLOUR_WINDOWFRAME))
 
         bSizer1 = wx.BoxSizer(wx.VERTICAL)
 
+        bSizer101 = wx.BoxSizer(wx.VERTICAL)
+
+        self.m_panel1 = wx.Panel(self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL)
+        bSizer12 = wx.BoxSizer(wx.VERTICAL)
+
         bSizer2 = wx.BoxSizer(wx.HORIZONTAL)
 
-        self.text_department = wx.StaticText(self, wx.ID_ANY, u"请输入部门名称", wx.DefaultPosition, wx.Size(150, 20),
+        self.text_department = wx.StaticText(self.m_panel1, wx.ID_ANY, u"请输入部门名称", wx.DefaultPosition, wx.Size(150, 20),
                                              wx.ALIGN_CENTRE)
         self.text_department.Wrap(-1)
         self.text_department.SetForegroundColour(wx.Colour(255, 255, 0))
@@ -40,14 +44,15 @@ class FrameZhuanli(wx.Frame):
 
         bSizer2.Add(self.text_department, 0, wx.ALL, 5)
 
-        self.input_department = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
+        self.input_department = wx.TextCtrl(self.m_panel1, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition,
+                                            wx.DefaultSize, 0)
         bSizer2.Add(self.input_department, 1, wx.ALL, 5)
 
-        bSizer1.Add(bSizer2, 0, wx.EXPAND, 5)
+        bSizer12.Add(bSizer2, 1, wx.EXPAND, 5)
 
         bSizer3 = wx.BoxSizer(wx.HORIZONTAL)
 
-        self.text_username = wx.StaticText(self, wx.ID_ANY, u"请输入用户名", wx.DefaultPosition, wx.Size(150, 20),
+        self.text_username = wx.StaticText(self.m_panel1, wx.ID_ANY, u"请输入用户名", wx.DefaultPosition, wx.Size(150, 20),
                                            wx.ALIGN_CENTRE)
         self.text_username.Wrap(-1)
         self.text_username.SetForegroundColour(wx.Colour(255, 255, 0))
@@ -55,14 +60,15 @@ class FrameZhuanli(wx.Frame):
 
         bSizer3.Add(self.text_username, 0, wx.ALL, 5)
 
-        self.input_username = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0)
+        self.input_username = wx.TextCtrl(self.m_panel1, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize,
+                                          0)
         bSizer3.Add(self.input_username, 1, wx.ALL, 5)
 
-        bSizer1.Add(bSizer3, 0, wx.EXPAND, 5)
+        bSizer12.Add(bSizer3, 0, wx.EXPAND, 5)
 
         bSizer4 = wx.BoxSizer(wx.HORIZONTAL)
 
-        self.text_password = wx.StaticText(self, wx.ID_ANY, u"请输入密码", wx.DefaultPosition, wx.Size(150, 20),
+        self.text_password = wx.StaticText(self.m_panel1, wx.ID_ANY, u"请输入密码", wx.DefaultPosition, wx.Size(150, 20),
                                            wx.ALIGN_CENTRE)
         self.text_password.Wrap(-1)
         self.text_password.SetForegroundColour(wx.Colour(255, 255, 0))
@@ -70,11 +76,18 @@ class FrameZhuanli(wx.Frame):
 
         bSizer4.Add(self.text_password, 0, wx.ALL, 5)
 
-        self.input_password = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize,
+        self.input_password = wx.TextCtrl(self.m_panel1, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize,
                                           wx.TE_PASSWORD)
         bSizer4.Add(self.input_password, 1, wx.ALL, 5)
 
-        bSizer1.Add(bSizer4, 0, wx.EXPAND, 5)
+        bSizer12.Add(bSizer4, 0, wx.EXPAND, 5)
+
+        self.m_panel1.SetSizer(bSizer12)
+        self.m_panel1.Layout()
+        bSizer12.Fit(self.m_panel1)
+        bSizer101.Add(self.m_panel1, 1, wx.EXPAND | wx.ALL, 5)
+
+        bSizer1.Add(bSizer101, 1, wx.EXPAND, 5)
 
         bSizer8 = wx.BoxSizer(wx.HORIZONTAL)
 
@@ -84,31 +97,39 @@ class FrameZhuanli(wx.Frame):
 
         self.test_info = wx.StaticText(self, wx.ID_ANY, u"请选择排除在外的状态", wx.DefaultPosition, wx.DefaultSize, 0)
         self.test_info.Wrap(-1)
-        self.test_info.SetForegroundColour(wx.Colour(255, 255, 0))
+        self.test_info.SetForegroundColour(wx.SystemSettings.GetColour(wx.SYS_COLOUR_INFOTEXT))
         self.test_info.SetBackgroundColour(wx.Colour(0, 128, 0))
 
         bSizer8.Add(self.test_info, 0, wx.ALL | wx.ALIGN_CENTER_HORIZONTAL, 5)
 
         bSizer10 = wx.BoxSizer(wx.HORIZONTAL)
 
-        self.checkbox_1 = wx.CheckBox(self, wx.ID_ANY, u"撤销", wx.DefaultPosition, wx.Size(-1, -1), 0)
+        self.m_panel2 = wx.Panel(self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL)
+        bSizer15 = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.checkbox_1 = wx.CheckBox(self.m_panel2, wx.ID_ANY, u"撤销", wx.DefaultPosition, wx.Size(-1, -1), 0)
         self.checkbox_1.SetValue(True)
-#        self.checkbox_1.SetFont(wx.Font(9, 70, 90, 90, False, "宋体"))
+        self.checkbox_1.SetFont(wx.Font(wx.NORMAL_FONT.GetPointSize(), 70, 90, 90, False, wx.EmptyString))
         self.checkbox_1.SetForegroundColour(wx.Colour(0, 255, 64))
 
-        bSizer10.Add(self.checkbox_1, 0, wx.ALL, 5)
+        bSizer15.Add(self.checkbox_1, 0, wx.ALL, 5)
 
-        self.checkbox_2 = wx.CheckBox(self, wx.ID_ANY, u"退回发起人", wx.DefaultPosition, wx.Size(-1, -1), 0)
+        self.checkbox_2 = wx.CheckBox(self.m_panel2, wx.ID_ANY, u"退回发起人", wx.DefaultPosition, wx.Size(-1, -1), 0)
         self.checkbox_2.SetValue(True)
         self.checkbox_2.SetForegroundColour(wx.Colour(0, 255, 64))
 
-        bSizer10.Add(self.checkbox_2, 0, wx.ALL, 5)
+        bSizer15.Add(self.checkbox_2, 0, wx.ALL, 5)
 
-        self.checkbox_3 = wx.CheckBox(self, wx.ID_ANY, u"驳回", wx.DefaultPosition, wx.Size(-1, -1), 0)
+        self.checkbox_3 = wx.CheckBox(self.m_panel2, wx.ID_ANY, u"驳回", wx.DefaultPosition, wx.Size(-1, -1), 0)
         self.checkbox_3.SetValue(True)
         self.checkbox_3.SetForegroundColour(wx.Colour(0, 255, 64))
 
-        bSizer10.Add(self.checkbox_3, 0, wx.ALL, 5)
+        bSizer15.Add(self.checkbox_3, 0, wx.ALL, 5)
+
+        self.m_panel2.SetSizer(bSizer15)
+        self.m_panel2.Layout()
+        bSizer15.Fit(self.m_panel2)
+        bSizer10.Add(self.m_panel2, 1, wx.EXPAND | wx.ALL, 5)
 
         bSizer8.Add(bSizer10, 0, wx.EXPAND, 5)
 
@@ -127,7 +148,7 @@ class FrameZhuanli(wx.Frame):
         bSizer91 = wx.BoxSizer(wx.VERTICAL)
 
         self.output_info = wx.TextCtrl(self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize,
-                                       wx.TE_MULTILINE)
+                                       wx.TE_MULTILINE | wx.TE_READONLY)
         bSizer91.Add(self.output_info, 1, wx.ALL | wx.EXPAND, 5)
 
         bSizer1.Add(bSizer91, 1, wx.EXPAND, 5)
@@ -162,10 +183,10 @@ class FrameZhuanli(wx.Frame):
         department_name_list = []
         type_invention_list = []
         shouli_sn_list = []
-        driverpath = os.path.join(os.path.abspath(os.path.curdir), "chromedriver.exe")
-        browser = webdriver.Chrome(driverpath)
-#        driverpath = os.path.join(os.path.abspath(os.path.curdir), "phantomjs.exe")
-#        browser = webdriver.PhantomJS(driverpath)
+#        driverpath = os.path.join(os.path.abspath(os.path.curdir), "chromedriver.exe")
+#        browser = webdriver.Chrome(driverpath)
+        driverpath = os.path.join(os.path.abspath(os.path.curdir), "phantomjs.exe")
+        browser = webdriver.PhantomJS(driverpath)
         url = "http://10.110.6.34/users/login"
         browser.get(url)
         browser.find_element_by_id("UserEmail").send_keys(username)
@@ -220,7 +241,7 @@ class FrameZhuanli(wx.Frame):
                     if data_status_display not in status_except_sub:
                         if data_status_display == '申请专利'.decode('gbk'):
                             WebDriverWait(browser, 100).until(ec.presence_of_element_located((By.CSS_SELECTOR, "#patents-related > div:nth-child(1) > span:nth-child(2) > table > tbody > tr > td:nth-child(1)")))
-                            shouli_sn = browser.find_element_by_css_selector("#patents-related > div:nth-child(1) > span:nth-child(2) > table > tbody > tr > td:nth-child(1)").text.strip()
+                            shouli_sn = browser.find_element_by_css_selector("#patents-related > div:nth-child(1) > span:nth-child(2) > table > tbody > tr > td:nth-child(3)").text.strip()
                             shouli_sn_list.append(shouli_sn)
                         else:
                             shouli_sn_list.append('None')
